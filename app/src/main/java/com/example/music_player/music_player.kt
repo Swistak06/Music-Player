d700f38cf0b7de3e458ac4e0229d7d536eada488
Łukasz Matysik <lukasz.matysik6@gmail.com>
2018-11-20 09:36:23 +0100

    Init

---
+++ app/src/main/java/com/example/music_player/music_player.kt
@@ -0,0 +1,12 @@
+package com.example.music_player
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class music_player : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_player)
+    }
+}

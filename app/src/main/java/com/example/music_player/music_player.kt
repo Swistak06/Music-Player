e721bfe5f50f8208c723f7c0964be4457a0e534c
Łukasz Matysik <lukasz.matysik6@gmail.com>
2018-11-21 09:49:37 +0100

    Changes

--- app/src/main/java/com/example/music_player/music_player.kt
+++ app/src/main/java/com/example/music_player/music_player.kt
@@ -2,11 +2,30 @@ package com.example.music_player
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_music_player.*
 
 class music_player : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_player)
+
+
+        seek_bar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                track_name.text = "Progress : $i"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
     }
+
+
 }

0438042a81c754d80c43ef5d485980069d6cbbbf
Łukasz Matysik <lukasz.matysik6@gmail.com>
2018-11-23 12:15:47 +0100

    Obsługa zmiany sekund

--- app/src/main/java/com/example/music_player/music_player.kt
+++ app/src/main/java/com/example/music_player/music_player.kt
@@ -1,22 +1,76 @@
 package com.example.music_player
 
-import android.support.v7.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.SeekBar
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_music_player.*
+import android.R.attr.orientation
+import android.content.res.Configuration
+
 
 class music_player : AppCompatActivity() {
+
+    private lateinit var mp: MediaPlayer
+    private val mSeekbarUpdateHandler = Handler()
+    private var trackMinutes = 0
+    private var trackSeconds = 0
+    private var maxTrackMinutes = 0
+    private var maxTrackSeconds = 0
+    private var currentTrack = R.raw.extacy
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_player)
+        if(mp == null)
+            mp = MediaPlayer.create (this, currentTrack)
+        maxTrackMinutes = (mp.duration/1000)/60
+        maxTrackSeconds = (mp.duration/1000)%60
+        track_time.text = "$maxTrackMinutes:$maxTrackSeconds"
+        var position = 0
+        play_button.setOnClickListener {
+            //Pausing player
+            if(mp.isPlaying ()) {
+                position = mp.getCurrentPosition ()
+                mp.pause ()
+                play_button.text = "Play"
+                track_name.text = mp.duration.toString()
+                mSeekbarUpdateHandler.removeCallbacks(mUpdateSeekbar)
+            }
+            //Resuming player
+            else if (position != 0)
+            {
+                mp.seekTo (position)
+                mp.start()
+                play_button.text = "Pause"
+                mSeekbarUpdateHandler.postDelayed(mUpdateSeekbar,0)
+            }
+            //Running player
+            else
+            {
+                seek_bar.max = mp.duration/1000
+                play_button.text = "Pause"
+                mp.start ()
+                mSeekbarUpdateHandler.postDelayed(mUpdateSeekbar,0)
+            }
 
+        }
 
         seek_bar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
 
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
-                track_name.text = "Progress : $i"
+                if(b){
+                    mp!!.seekTo(i*1000)
+                }
+                trackMinutes = i / 60
+                trackSeconds = i % 60
+                if(trackSeconds < 10 )
+                    current_track_time.text = "$trackMinutes:0$trackSeconds"
+                else
+                    current_track_time.text = "$trackMinutes:$trackSeconds"
+
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {
@@ -27,5 +81,23 @@ class music_player : AppCompatActivity() {
         })
     }
 
+    private val mUpdateSeekbar = object : Runnable {
+        override fun run() {
+            //track_name.text = mp.currentPosition.toString()
+            seek_bar.progress = mp.getCurrentPosition()/1000
+            mSeekbarUpdateHandler.postDelayed(this, 1000)
+        }
+    }
 
+
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+    }
 }
